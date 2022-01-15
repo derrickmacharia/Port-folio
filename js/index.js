1503bb2f29b7a7998e938ee502feff92eaaad172
derrickmacharia <derrick.macharia@student.moringaschool.com>
2022-01-16 00:33:39 +0300

    create home section

--- js/index.js
+++ js/index.js
@@ -1,51 +1,59 @@
 
-// ///////-------------/////
-// var cursor = document.querySelector('.cursor'),
-// cursorScale = document.querySelectorAll('.cursor-scale'),
-//     mouseX = 0,
-//     mouseY = 0
+  
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-// gsap.to({}, 0.016, {
-//     repeat: -1,
-
-//     onRepeat: function() {
-//         gsap.set(cursor, {
-//             css: {
-//                 left: mouseX,
-//                 top: mouseY
-
-//             }
-//         })
-//     }
-// });
-
-// window.addEventListener('mousemove', function (e){
-//     mouseX = e.clientX;
-//     mouseY = e.clientY
-// })
-
-// cursorScale.forEach(link => {
-//     link.addEventListener('mouseleave', () => {
-//         cursor.classList.remove('grow');
-//         cursor.classList.remove('grow-small');
-//     });
-//     link.addEventListener('mousemove', () => {
-//         cursor.classList.add('grow');
-//         if(link.classList.contains('small')){
-//             cursor.classList.remove('grow');
-//             cursor.classList.add('grow-small');
-//         }
-//     });
-// })
-
-const list = document.querySelectorAll('.list');
-
-function activeLink(){
-    list.forEach((item) => 
-    item.classList.remove('active'));
-    this.classList.add('active');
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-list.forEach((item) =>
-item.addEventListener('click', activeLink));
 
-// const list = document.querySelectorAll('.list')
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
